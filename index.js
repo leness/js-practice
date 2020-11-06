fc043d654b4d78fe9d19041d857866f1eafdc0cc
Olena Stadnyk <lenesx@gmail.com>
2020-11-06 16:00:55 +0200

    need to validate method

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-// const incBtn = document.getElementById("increment");
-// const decBtn = document.getElementById("decrement");
-// const countSpan = document.getElementById("count");
+const incBtn = document.getElementById("increment");
+const decBtn = document.getElementById("decrement");
+const countSpan = document.getElementById("count");
 
-// class Counter {
-//   constructor(count, value) {
-//     this.count = count;
-//     this.value = value;
-//   }
+class Counter {
+  constructor(count, value) {
+    this.count = count;
+    this.value = value;
+  }
 
-//   toIncrement() {
-//     this.count += this.value;
-//     countSpan.textContent = this.count;
-//     return this.count;
-//   }
-//   toDecrement() {
-//     if (this.count <= this.value) return;
-//     this.count -= this.value;
-//     countSpan.textContent = this.count;
+  toIncrement() {
+    this.count += this.value;
+    countSpan.textContent = this.count;
+    return this.count;
+  }
+  toDecrement() {
+    if (this.count <= this.value) return;
+    this.count -= this.value;
+    countSpan.textContent = this.count;
 
-//     return this.count;
-//   }
-// }
+    return this.count;
+  }
+}
 
-// const myCounter = new Counter(5, 5);
-// console.log(myCounter);
+const myCounter = new Counter(5, 5);
+console.log(myCounter);
 
-// decBtn.addEventListener("click", () => {
-//   myCounter.toDecrement();
-//   console.log(myCounter.count);
-// });
+decBtn.addEventListener("click", () => {
+  myCounter.toDecrement();
+  console.log(myCounter.count);
+});
 
-// incBtn.addEventListener("click", () => {
-//   myCounter.toIncrement();
-//   console.log(myCounter.count);
-// });
+incBtn.addEventListener("click", () => {
+  myCounter.toIncrement();
+  console.log(myCounter.count);
+});
 
-// const myNewCounter = new Counter(5, 8);
-// console.log(myNewCounter);
+const myNewCounter = new Counter(5, 8);
+console.log(myNewCounter);
 
 
 
